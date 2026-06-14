369dc6d90552ee10c1e5fa0b51bd4be69514d94d
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2026-06-13 23:17:53 -0500

    conexion de backend con frontend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://horizon-vptj.vercel.app/live",
     methods: ["GET", "POST"]
   }
 });
